88a5ecbf75e8d27049f10c6132e7763b9874e192
KeziAntero <keziantero@gmail.com>
2020-09-30 21:38:33 -0300

    Iniciando

--- .history/tempo_20200930205508.js
+++
@@ -1,5 +0,0 @@
-const notificationElement = document.querySelector(".notification");
-const iconElement = document.querySelector(".weather-icon");
-const iconElement = document.querySelector(".temperature-value p");
-const iconElement = document.querySelector(".temperature-description p");
-const iconElement = document.querySelector(".location p");
